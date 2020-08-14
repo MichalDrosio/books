63034f131ea3876f5b30794a15212809388b1b48
mordziak <drosio.michal@gmail.com>
2020-08-14 16:03:45 +0200

    category and authors

--- forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from books.models import ValueVotesBook
-
-
-class ValueVoteFrom(forms.ModelForm):
-    class Meta:
-        model = ValueVotesBook
-        fields = ('value_vote',)
